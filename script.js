c878517587bd3e66075829467879cf730d133722
elenvix <151098187+elenvix@users.noreply.github.com>
2024-03-29 15:49:00 -0600

    jss

--- script.js
+++ script.js
@@ -1,13 +1,6 @@
-// script.js
+
 document.addEventListener('DOMContentLoaded', function () {
-    const isLoggedIn = localStorage.getItem('isLoggedIn');
-    if (!isLoggedIn) {
-        // Si no está autenticado, redirige a la página de inicio de sesión
-        window.location.href = 'login.html';
-    } else {
-        // Si está autenticado, carga el inventario
-        cargarProductos();
-    }
+    cargarProductos();
 });
 
 function cargarProductos() {
@@ -28,29 +21,49 @@ function cargarProductos() {
             <td>${producto.cantidad}</td>
             <td>${producto.cantidad_maxima}</td>
             <td>${producto.estado}</td>
+            <td><button class="btn btn-danger" onclick="eliminarProducto(${producto.id})">Eliminar</button></td>
         `;
         listaProductos.appendChild(tr);
     });
-};
+}
 
-// script.js
-document.addEventListener('DOMContentLoaded', function() {
-    const loginBtn = document.getElementById('loginBtn');
-    loginBtn.addEventListener('click', function() {
-        login();
-    });
-});
+function agregarProducto() {
+    const codigo = document.getElementById('inputCodigo').value;
+    const nombre = document.getElementById('inputNombre').value;
+    const cantidad = document.getElementById('inputCantidad').value;
+    const cantidadMaxima = document.getElementById('inputCantidadMaxima').value;
+
+    const nuevoProducto = {
+        id: Math.floor(Math.random() * 1000) + 1,
+        codigo: codigo,
+        nombre: nombre,
+        cantidad: cantidad,
+        cantidad_maxima: cantidadMaxima,
+        estado: cantidad == cantidadMaxima ? "Disponible" : "Por rellenar"
+    };
+
+    const listaProductos = document.getElementById('lista-productos');
+    const tr = document.createElement('tr');
+    tr.innerHTML = `
+        <td>${nuevoProducto.id}</td>
+        <td>${nuevoProducto.codigo}</td>
+        <td>${nuevoProducto.nombre}</td>
+        <td>${nuevoProducto.cantidad}</td>
+        <td>${nuevoProducto.cantidad_maxima}</td>
+        <td>${nuevoProducto.estado}</td>
+        <td><button class="btn btn-danger" onclick="eliminarProducto(${nuevoProducto.id})">Eliminar</button></td>
+    `;
+    listaProductos.appendChild(tr);
+}
+
+function eliminarProducto(id) {
+    const productoAEliminar = document.getElementById(id);
+    productoAEliminar.remove();
+}
 
 function login() {
-    const usuario = document.getElementById('usuario').value;
-    const contraseña = document.getElementById('contraseña').value;
-
-    // Verificar las credenciales del usuario
-    if (usuario === 'ale' && contraseña === '142536') {
-        // Credenciales válidas, redirigir a otra página (en este caso, index.html)
-        window.location.href = 'index.html';
-    } else {
-        // Credenciales inválidas, mostrar mensaje de error
-        alert('Usuario o contraseña incorrectos');
-    }
+    // Aquí iría la lógica de autenticación
+    // Por ejemplo, verificar el usuario y la contraseña
+    // Si son válidos, redirige a index.html
+    window.location.href = 'index.html';
 }
